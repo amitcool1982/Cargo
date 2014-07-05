06810ed8f2c9c8f6c77775620a87d51c3a406abd
amitcool1982 <amitcool1982@gmail.com>
2014-07-06 00:19:14 +0530

    Add ItemPromotion page code.

---
+++ Cargo/ItemPromotion.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Cargo.Master" AutoEventWireup="true" CodeBehind="ItemPromotion.aspx.cs" Inherits="Cargo.ItemPromotion" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <link href="Content/bootstrap.css" rel="stylesheet">
+    <link href="Content/site.css" rel="stylesheet" />
+    <link href="Content/sb-admin.css" rel="stylesheet">
+    <link href="Content/font-awesome.css" rel="stylesheet">
+
+    <script src="Scripts/site.js"></script>
+    <script type="text/javascript" src="../Scripts/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="../Scripts/DT_bootstrap.js"></script>
+    <script type="text/javascript">
+        var PageSize = "<%=PageSize%>";
+    </script>
+    <script type="text/javascript" src="../Scripts/ItemPromotion/ItemPromotion.js"></script>
+
+
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="wrapper">
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Promotion <small>Configuration</small></h1>
+                    <ol class="breadcrumb">
+                        <li><a href="Dashboard.aspx"><i class="icon-dashboard"></i>Dashboard</a></li>
+                        <li class="active">Promotion</li>
+                    </ol>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-9">
+                    <div class="form-group">
+                        <label>Search:</label>
+                        <input id="txtSearch" type="text" />
+                    </div>
+                    <table id="myTableItemPromotion" class="dataTables_wrapper form-inline" role="grid">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Item</th>
+                                <th>Promo</th>                                
+                                <th>Dsc(%)</th>
+                                <th>Start Promos</th>                                
+                                <th>End Promo</th>
+                                <th><i class="fa fa-user"></i></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-lg-3">
+                    <div>
+                        <h3>Form Item</h3>
+                        <input name="id" value="" type="hidden">
+                        <input name="activate" value="" type="hidden">
+                        <div class="form-group">
+                            <label for="id">ID Item :</label>
+                            <input id="id" required="" name="id_menu" placeholder="Type ID Item here.." class="form-control" type="text">
+                            <!-- <p class="help-block" style="font-size:11px;"></p> -->
+                        </div>
+                        <div class="form-group">
+                            <label for="type">Promo :</label>
+                            <select id="type" name="type_promo" class="form-control" style="width: 100%;">
+                                <option selected="selected" value="">- Promo Type -</option>
+                                <option value="od">Discount Promo</option>
+                                <!-- <option value="dso">Discount &amp; Special Offer</option>
+                            <option value="so">Special Offer Promo</option> -->
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description Promo :</label>
+                            <textarea id="description" name="description_promo" class="form-control" placeholder="Description promo or special offer" style="width: 100%;"></textarea>
+                            <p class="help-block" style="font-size: 11px;">Tips: Make sure this special offer description matched with your real promotion</p>
+                        </div>
+                        <div class="form-group">
+                            <label for="discount">Discount :</label>
+                            <input id="discount" min="0" max="100" step="5" name="discount" class="form-control" placeholder="discount" style="width: 100px;" type="number">
+                        </div>
+                        <div class="form-group">
+                            <label for="start">Start Promo at :</label>
+                            <input id="start" name="start_promo" class="form-control" placeholder="Start promo datetime" style="width: 100%;" type="datetime-local">
+                        </div>
+                        <div class="form-group">
+                            <label for="end">End Promo at :</label>
+                            <input id="end" name="end_promo" class="form-control" placeholder="End promo datetime" style="width: 100%;" type="datetime-local">
+                        </div>
+                        <br>
+                        <button type="submit" class="btn-promo btn btn-success">Submit</button>
+                        <button type="button" class="promotion-show-form btn btn-danger" style="display: none;">Cancel</button>
+                    </div>
+                </div>
+            </div>
+
+            <br />
+            <div id="errordiv"></div>
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+</asp:Content>
---
+++ Cargo/ItemPromotion.aspx.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Net;
+using System.Xml;
+using System.Globalization;
+using System.Web.Security;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Collections;
+using System.IO;
+using System.Web.Script;
+using Cargo.BLL;
+
+namespace Cargo
+{
+    public partial class ItemPromotion : System.Web.UI.Page
+    {
+        static int sEcho = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        object GetSQLSafeValue(object obj)
+        {
+            if (obj == null)
+                return DBNull.Value;
+            else if (obj.ToString().Length == 0)
+                return DBNull.Value;
+            else
+                return obj;
+        }
+
+
+
+        public DataTable GetItemPromotion(int PageIndex, int PageSize, string SearchFilter, string SortBy, int SortDirection)
+        {
+            string strConnectionStrings = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString1"].ConnectionString;
+            SqlParameter[] oParam = new SqlParameter[5];
+            oParam[0] = DBHelper.GetParam("@piPageSize", SqlDbType.Int, 4, ParameterDirection.Input, GetSQLSafeValue(1000));
+            oParam[1] = DBHelper.GetParam("@piPageNumber", SqlDbType.Int, 4, ParameterDirection.Input, GetSQLSafeValue(PageIndex));
+            oParam[2] = DBHelper.GetParam("@piSortedBy", SqlDbType.VarChar, 20, ParameterDirection.Input, GetSQLSafeValue(SortBy));
+            oParam[3] = DBHelper.GetParam("@piSearchFilter", SqlDbType.VarChar, -1, ParameterDirection.Input, SearchFilter);
+            oParam[4] = DBHelper.GetParam("@piSortDirection", SqlDbType.Int, 4, ParameterDirection.Input, GetSQLSafeValue(SortDirection));
+            DataTable otable = SqlHelper.ExecuteDataset(strConnectionStrings, CommandType.StoredProcedure, "USP_GetItemPromotion", oParam).Tables[0];
+
+            return otable;
+        }
+
+        public int PageSize
+        {
+            get
+            {                
+                return 20;                
+            }
+            set { Session["PageSize"] = Convert.ToString(value); }
+        }
+
+        [WebMethod(EnableSession = true)]
+        [System.Web.Script.Services.ScriptMethod(ResponseFormat = System.Web.Script.Services.ResponseFormat.Json, UseHttpGet = false)]
+        public static string GetItemPromotion(int PageIndex, int PageSize, string SortCol, string SortDir, string SearchFilter)
+        {
+            ItemPromotion objItemPromotion = new ItemPromotion();
+            Hashtable objHT = new Hashtable();
+            string totalRecords = "";
+            string totalDisplayRecords = "";
+            int count = 0;
+            string rowClass = "";
+            var sb = new StringBuilder();
+            string outputJson = string.Empty;
+            DataTable dtItemPromotion = new DataTable();
+            string SortBy = "id";
+            int SortDirection = 1;
+            if (SortCol == "0")
+            {
+                SortBy = "id";
+            }
+            else if (SortCol == "1")
+            {
+                SortBy = "nama_menu";
+            }
+            else if (SortCol == "2")
+            {
+                SortBy = "tipe_promo";
+            }
+            else if (SortCol == "3")
+            {
+                SortBy = "diskon";
+            }
+            else if (SortCol == "4")
+            {
+                SortBy = "mulai_promo";
+            }
+            else if (SortCol == "5")
+            {
+                SortBy = "akhir_promo";
+            }
+            
+
+            if (SortDir == "asc")
+            {
+                SortDirection = 1;
+            }
+            else
+            {
+                SortDirection = 0;
+            }
+            dtItemPromotion = objItemPromotion.GetItemPromotion(PageIndex, PageSize, SearchFilter, SortBy, SortDirection);
+            if (dtItemPromotion.Rows.Count > 0)
+            {
+                for (int i = 0; i < dtItemPromotion.Rows.Count; i++)
+                {
+                    if (totalRecords.Length == 0)
+                    {
+                        totalRecords = dtItemPromotion.Rows[i]["TotalCount"].ToString();
+                        totalDisplayRecords = dtItemPromotion.Rows[i]["TotalCount"].ToString();
+                    }
+                    sb.Append("{");
+                    sb.AppendFormat(@"""id"": ""{0}""", count++);
+                    sb.Append(",");
+                    sb.AppendFormat(@"""DT_RowClass"": ""{0}""", rowClass);
+                    sb.Append(",");
+                    sb.AppendFormat(@"""0"": ""{0}""", "<div style='text-overflow: ellipsis; width: 50px;overflow: hidden;'><nobr>" + dtItemPromotion.Rows[i]["id"].ToString().Replace("\"", "\\" + "\"") + "</nobr></div>");
+                    sb.Append(",");
+
+                    sb.AppendFormat(@"""1"": ""{0}""", "<div style='text-overflow: ellipsis; width: 100px;overflow: hidden;'><nobr>" + dtItemPromotion.Rows[i]["nama_menu"].ToString().Replace("\"", "\\" + "\"") + "</nobr></div>");
+                    sb.Append(",");
+
+                    sb.AppendFormat(@"""2"": ""{0}""", "<div style='text-overflow: ellipsis; width: 100px;overflow: hidden;'><nobr>" + dtItemPromotion.Rows[i]["tipe_promo"].ToString().Replace("\"", "\\" + "\"") + "</nobr></div>");
+                    sb.Append(",");
+
+                    sb.AppendFormat(@"""3"": ""{0}""", "<div style='text-overflow: ellipsis; width: 40px;overflow: hidden;'><nobr>" + dtItemPromotion.Rows[i]["diskon"].ToString().Replace("\"", "\\" + "\"") + "</nobr></div>");
+                    sb.Append(",");
+
+                    sb.AppendFormat(@"""4"": ""{0}""", "<div style='text-overflow: ellipsis; width: 140px;overflow: hidden;'><nobr>" + dtItemPromotion.Rows[i]["mulai_promo"].ToString().Replace("\"", "\\" + "\"") + "</nobr></div>");
+                    sb.Append(",");
+
+                    sb.AppendFormat(@"""5"": ""{0}""", "<div style='text-overflow: ellipsis; width: 140px;overflow: hidden;'><nobr>" + dtItemPromotion.Rows[i]["akhir_promo"].ToString().Replace("\"", "\\" + "\"") + "</nobr></div>");
+                    sb.Append(",");
+                   
+                    sb.AppendFormat(@"""6"": ""{0}""", "<div><a class='edit' href='javascript:void(0)'><i class='fa fa-edit fa-border'></i></a><a class='delete' href='javascript:void(0)'><i class='fa fa-trash-o fa-border'></i></a></div>");
+                    sb.Append("},");
+                }
+
+                outputJson = sb.Remove(sb.Length - 1, 1).ToString();
+                sb.Clear();
+                sb.Append("{");
+                sb.Append(@"""sEcho"": ");
+                sb.AppendFormat(@"""{0}""", sEcho++);
+                sb.Append(",");
+                sb.Append(@"""iTotalRecords"": ");
+                sb.Append(totalRecords);
+                sb.Append(",");
+                sb.Append(@"""iTotalDisplayRecords"": ");
+                sb.Append(totalDisplayRecords);
+                sb.Append(", ");
+                sb.Append(@"""aaData"": [ ");
+                sb.Append(outputJson);
+                sb.Append("]}");
+                outputJson = sb.ToString();
+            }
+            else
+            {
+                sb.Append("{");
+                sb.Append(@"""sEcho"": ");
+                sb.AppendFormat(@"""{0}""", sEcho++);
+                sb.Append(",");
+                sb.Append(@"""iTotalRecords"": 0");
+                sb.Append(",");
+                sb.Append(@"""iTotalDisplayRecords"": 0");
+                sb.Append(", ");
+                sb.Append(@"""aaData"": [ ");
+                sb.Append("]}");
+                outputJson = sb.ToString();
+            }
+            return outputJson;
+        }
+
+        public static int AddItemPromotion(string Name, string Alias)
+        {
+            string strConnectionStrings = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString1"].ConnectionString;
+            SqlParameter[] oParam = new SqlParameter[2];
+            oParam[0] = DBHelper.GetParam("@Name", SqlDbType.VarChar, 100, ParameterDirection.Input, Name);
+            oParam[1] = DBHelper.GetParam("@Alias", SqlDbType.VarChar, 100, ParameterDirection.Input, Alias);
+            SqlHelper.ExecuteNonQuery(strConnectionStrings, CommandType.StoredProcedure, "USP_AddItemCategory", oParam);
+            return 1;
+        }
+
+        [WebMethod(EnableSession = true)]
+        [System.Web.Script.Services.ScriptMethod(ResponseFormat = System.Web.Script.Services.ResponseFormat.Json, UseHttpGet = false)]
+        public static int AddItemPromotions(string Name, string Alias)
+        {
+            return AddItemPromotion(Name, Alias);
+        }
+    
+    
+    }
+}
