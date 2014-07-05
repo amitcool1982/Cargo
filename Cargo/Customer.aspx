c23640a0a0b521c94891bc55d20b7e13ec95197b
amitcool1982 <amitcool1982@gmail.com>
2014-07-05 14:25:14 +0530

    Add customer page & SqlHelper & DBHelper class.

---
+++ Cargo/Customer.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Cargo.Master" AutoEventWireup="true" CodeBehind="Customer.aspx.cs" Inherits="Cargo.Customer" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="Content/site.css" rel="stylesheet" />
+    <link href="../Content/select2.css" rel="stylesheet" />
+
+    <script type="text/javascript" src="../Scripts/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="../Scripts/DT_bootstrap.js"></script>
+    <script src="../Scripts/select2.js"></script>
+    <script type="text/javascript">
+        var PageSize = "<%=PageSize%>";
+    </script>
+    <script type="text/javascript" src="../Scripts/Customer/Customer.js"></script>
+
+
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="wrapper">
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Customer <small>Information List</small></h1>
+                    <ol class="breadcrumb">
+                        <li><a href="Dashboard.aspx"><i class="icon-dashboard"></i>Dashboard</a></li>
+                        <li class="active"><i class="icon-file-alt"></i>Customer</li>
+                    </ol>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-8">
+                    <table id="myTableCustomer" style="border-style: dotted; border-width: .5px;">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Email</th>
+                                <th>Full Name</th>
+                                <th>Phone</th>
+                                <th>City</th>
+                                <th>Join</th>
+                                <th>Last Login</th>
+                                <th><i class="fa fa-user"></i></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <br />
+            <div id="errordiv"></div>
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+</asp:Content>
---
+++ Cargo/Scripts/Customer/Customer.js
@@ -0,0 +1,76 @@
+﻿$(document).ready(function () {
+    BindCustomerTable();
+});
+
+var BindCustomerTable = function () {
+
+
+    return $('#myTableCustomer').dataTable({
+        "oLanguage": {
+            "sZeroRecords": "No records to display"//,
+
+        },
+        "aoColumns": [
+                     { "sWidth": "2em", "bSortable": true },
+                     { "sWidth": "8em", "bSortable": true },
+                     { "sWidth": "8em", "bSortable": true },
+                     { "sWidth": "5em", "bSortable": true },
+                     { "sWidth": "8em", "bSortable": true },
+                     { "sWidth": "8em", "bSortable": true },
+                     { "sWidth": "8em", "bSortable": true },                     
+                     { "sWidth": "5em", "bSortable": false }
+        ],
+        "bProcessing": true,
+        "bServerSide": true,
+        "bStateSave": false,
+        "bPaginate": true,
+        "bLengthChange": false,//To Hide the Length menu in the Datatables
+        "iDisplayLength": Number(PageSize),
+        "bAutoWidth": false,
+        "bDestroy": true,
+        "bDeferRender": true,
+        "bFilter": true,
+        "bSort": true,
+        "sPaginationType": "bs_normal",
+        "sAjaxSource": "Customer.aspx/GetCustomers",
+
+        "fnServerData": function (sSource, aoData, fnCallback) {
+
+            var data = "{ ";
+
+            for (var i = 0; i < aoData.length; i++) {
+                if (aoData[i].name == 'iDisplayLength') {
+                    aoData.push({ "name": "PageSize", "value": aoData[i].value });
+                }
+                if (aoData[i].name == 'iDisplayStart') {
+                    aoData.push({ "name": "PageIndex", "value": ((aoData[i].value) + 1) });
+                }
+
+                data += aoData[i].name + ": ";
+                data += "'" + aoData[i].value + "'";
+
+                if (i != (aoData.length - 1))
+                    data += ", ";
+            }
+
+            data += " }";
+            $.ajax({
+                type: "POST",
+                url: sSource,
+                data: data,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (msg) {
+                    if (msg.d != null) {
+                        var json = jQuery.parseJSON(msg.d);
+                        fnCallback(json);
+                        $("#myTableCustomer").show();
+                    }
+
+                }
+            });
+        }
+
+    });
+
+}
