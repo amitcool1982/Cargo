33cb4d67293eafa44e8bea27f7de609154d67fef
amitcool1982 <amitcool1982@gmail.com>
2014-07-09 11:57:45 +0530

    Add production configurable file.

---
+++ Cargo/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using Cargo.BLL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cargo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        object GetSQLSafeValue(object obj)
+        {
+            if (obj == null)
+                return DBNull.Value;
+            else if (obj.ToString().Length == 0)
+                return DBNull.Value;
+            else
+                return obj;
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string strConnectionStrings = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString1"].ConnectionString;
+            SqlParameter[] oParam = new SqlParameter[2];
+            oParam[0] = DBHelper.GetParam("@UserName", SqlDbType.VarChar, 50, ParameterDirection.Input, GetSQLSafeValue("super_admin"));
+            oParam[1] = DBHelper.GetParam("@Password", SqlDbType.VarChar, 50, ParameterDirection.Input, GetSQLSafeValue("admin"));
+            DataTable otable = SqlHelper.ExecuteDataset(strConnectionStrings, CommandType.StoredProcedure, "USP_ValidateLogin", oParam).Tables[0];
+
+        }
+    }
+}
