85336ce50e5cdffceb39e889e956f55a7232a971
amitcool1982 <amitcool1982@gmail.com>
2014-07-10 16:44:02 +0530

    Fix issue in delete item recommended.

--- SQL Scripts/USP_DeleteItemRecommend.sql
+++ SQL Scripts/USP_DeleteItemRecommend.sql
@@ -1,11 +1,11 @@
 
 
-IF EXISTS(SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[USP_GetItemRecommendDetails]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [dbo].USP_GetItemRecommendDetails
+IF EXISTS(SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[USP_DeleteItemRecommend]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].USP_DeleteItemRecommend
 GO
   SET ANSI_NULLS, QUOTED_IDENTIFIER ON;
  GO
-CREATE PROCEDURE [dbo].USP_GetItemRecommendDetails   
+CREATE PROCEDURE [dbo].USP_DeleteItemRecommend   
 (
 	@Id BigInt=0
  )     
