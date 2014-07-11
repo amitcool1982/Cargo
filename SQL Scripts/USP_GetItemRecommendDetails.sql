1f8f9950d2ffe4aecd515d982178b95060925c94
amitcool1982 <amitcool1982@gmail.com>
2014-07-11 15:46:45 +0530

    fix bug in recommend data column.

--- SQL Scripts/USP_GetItemRecommendDetails.sql
+++ SQL Scripts/USP_GetItemRecommendDetails.sql
@@ -11,6 +11,17 @@ CREATE PROCEDURE [dbo].USP_GetItemRecommendDetails
 AS   
 BEGIN 	
 	
-		Select	* from [ledb_menu] C With(nolock)	Where	id	= @id 	
+		select			Item.[id], Item.menu_id_generator,	Item.id_vendors,	Item.alias_menu,	Item.nama_menu, Item.deskripsi_menu, 
+		
+		--Item.media_photo, -- right now image not uploaded in database
+		
+		'/Images/no_picture_wide.png' As media_photo,
+		
+		Item.harga_menu, Item.kategori_menu, Item.tags, Item.is_recommended, Item.last_update, Vendors.nama_vendors, ItemCat.nama_kategori As Category
+		from 
+		ledb_menu			Item	With(nolock)	Inner Join
+		ledb_vendors		Vendors	With(nolock)	On Item.id_vendors = Vendors.id_vendors_generator	Inner Join
+		ledb_kategorimenu	ItemCat With(nolock)	On Item.kategori_menu = ItemCat.alias
+		Where	Item.id	= @id 	
 	   
 END  
